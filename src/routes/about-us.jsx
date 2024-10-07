ec23c722516a89d884c2febdde0e3e53c3b8f11a
Ingirorhaun <francescopantusa@gmail.com>
2024-10-07 14:23:56 +0200

    add about-us page

--- src/routes/about-us.jsx
+++ src/routes/about-us.jsx
@@ -1,8 +1,36 @@
+import "./../styles/pages/_about-us.scss";
+import { Dropdown } from "../components/dropdown";
+
 export default function AboutUs() {
-    return (
-        <>
-            <h1>About Us</h1>
-            <p>This is the about us page</p>
-        </>
-    );
+  return (
+    <>
+      <section className="hero" id="about-hero"></section>
+      <section id="about-content">
+        <Dropdown
+          title={"Fiabilité"}
+          content={
+            "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+          }
+        />
+        <Dropdown
+          title={"Respect"}
+          content={
+            "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          }
+        />
+        <Dropdown
+          title={"Service"}
+          content={
+            "La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
+          }
+        />
+        <Dropdown
+          title={"Sécurité"}
+          content={
+            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour nos locataires, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos equipes de vérifier que les standards sont bien respectés. Nous organisions également sur la sécurité domestique pour nos hôtes."
+          }
+        />
+      </section>
+    </>
+  );
 }
