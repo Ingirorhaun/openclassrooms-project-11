e783d0f2f30f903167ff420d26490dce52c8c557
Ingirorhaun <francescopantusa@gmail.com>
2024-10-07 14:21:01 +0200

    add header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,18 @@
+import { Logo } from "./logo";
+import "./../styles/components/_header.scss";
+
 export default function Header() {
-    return (
-        <header>
-            <p>KASA header</p>
-        </header>
-    );
+  const pathname = window.location.pathname;
+
+  return (
+    <header>
+      <Logo />
+      <nav>
+        <a href="/" className={pathname === "/" ? "active" : ""}>Accueil</a>
+        <a href="/about" className={pathname === "/about" ? "active" : ""}>
+          A propos
+        </a>
+      </nav>
+    </header>
+  );
 }
