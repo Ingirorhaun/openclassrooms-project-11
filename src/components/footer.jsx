e20f4d96209999179cb6d56e08bff841e0ceb350
Ingirorhaun <francescopantusa@gmail.com>
2024-10-07 14:21:15 +0200

    add footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,11 @@
+import "./../styles/components/_footer.scss";
+import { Logo } from "./logo";
+
 export default function Footer() {
-    return (
-        <footer>
-            <p>footer</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <Logo color="#FFF"/>
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
 }
